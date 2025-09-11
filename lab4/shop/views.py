3437501f364a13302a2e51a5f5f0d82b28e1ba25
Punisher385 <vasykhnikdenus@gmail.com>
2025-09-12 02:52:07 +0300

    Lab 4: створено 5 вюшок і шаблони для магазину

--- lab4/shop/views.py
+++ lab4/shop/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, "shop/home.html", {"title": "Магазин одягу"})
+    return render(request, "shop/home.html")
 
 def men(request):
-    return render(request, "shop/men.html", {"title": "Чоловічий одяг"})
+    return render(request, "shop/men.html")
 
 def women(request):
-    return render(request, "shop/women.html", {"title": "Жіночий одяг"})
+    return render(request, "shop/women.html")
 
 def kids(request):
-    return render(request, "shop/kids.html", {"title": "Дитячий одяг"})
+    return render(request, "shop/kids.html")
 
 def about(request):
-    return render(request, "shop/about.html", {"title": "Про магазин"})
+    return render(request, "shop/about.html")
+
